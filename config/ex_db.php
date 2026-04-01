8cb58552a39bc4c87849d0dc42aba022538f95f3
666pupsik777 <pospelova.02.10.06@mail.ru>
2026-04-01 12:24:33 +0700

    end 1

---
+++ config/ex_db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'db',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
